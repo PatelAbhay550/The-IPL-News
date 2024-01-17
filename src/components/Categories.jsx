d7ee818cbe52d7374d309f78c689779a2e894ec5
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2024-01-17 22:05:22 +0530

    Update Categories.jsx

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,7 +4,7 @@ import "../App.css";
 const Categories = () => {
   return (
     <div className="w-full h-10 border-b-2 border-white ">
-      <ul className=" w-full flex gap-10 sm:gap-14 justify-center font-bold font-sans m-0 sm:text-lg text-zinc-800 text-xs ">
+      <ul className=" w-full cat flex gap-10 sm:gap-14 justify-center font-bold font-sans m-0 sm:text-lg text-zinc-800 text-xs ">
         <li>MATCHES</li>
         <li>STATS</li>
         <li>AUCTION</li>
