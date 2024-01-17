1b58e25adea83bb32fd9f51b77cf0624f748a053
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2024-01-17 19:04:21 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDTXH3OW1EUbee8yXt04YkC7zvTWfCt1Ts",
+  authDomain: "myblog-aee44.firebaseapp.com",
+  projectId: "myblog-aee44",
+  storageBucket: "myblog-aee44.appspot.com",
+  messagingSenderId: "851291435958",
+  appId: "1:851291435958:web:b7767c42b275579bf74717",
+  measurementId: "G-D7D8PWJ936"
 };
 
 // Initialize Firebase
