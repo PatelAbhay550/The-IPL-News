7aa189e4d64e72077eda8cbf7ac9b3f27bebd6da
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2024-01-18 12:59:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import prerender from "prerender-node";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      // Add prerender middleware
+      configureServer: ({ middlewares }) => {
+        middlewares.use(prerender({ prerenderToken: "Y3vVUs1l2t1Avn6xZMLM" }));
+      },
+    },
+  ],
+});
